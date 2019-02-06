1705ab961c64d0b201c32474f274d026d644c5b5
Haya Akkad <haya.akkad94@gmail.com>
2019-02-06 19:43:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/homepharmacyproject/Activity/RegistrationActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.homepharmacyproject.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.homepharmacyproject.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
---
+++ app/src/main/java/com/example/android/homepharmacyproject/Adapter/ImageAdapter.java
@@ -0,0 +1,62 @@
+package com.example.android.homepharmacyproject.Adapter;
+
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.android.homepharmacyproject.R;
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+    private final String[] gridViewString;
+    private final int[] gridViewImageId;
+
+    public ImageAdapter(Context context, String[] gridViewString, int[] gridViewImageId) {
+        mContext = context;
+        this.gridViewImageId = gridViewImageId;
+        this.gridViewString = gridViewString;
+    }
+
+    @Override
+    public int getCount() {
+        return gridViewString.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup parent) {
+        View gridViewAndroid;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+
+            gridViewAndroid = new View(mContext);
+            gridViewAndroid = inflater.inflate(R.layout.home_item, null);
+            TextView textViewAndroid = (TextView) gridViewAndroid.findViewById(R.id.android_gridview_text);
+            ImageView imageViewAndroid = (ImageView) gridViewAndroid.findViewById(R.id.android_gridview_image);
+            textViewAndroid.setText(gridViewString[i]);
+            imageViewAndroid.setImageResource(gridViewImageId[i]);
+        } else {
+            gridViewAndroid = (View) convertView;
+        }
+
+        return gridViewAndroid;
+    }
+}
